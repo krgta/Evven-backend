0b9f8bb77691ea9c6b1df024ed504ece4f280bcb
jagdep-singh <jagdeep.singh12a@gmail.com>
2026-05-20 01:57:07 +0530

    added a html page for /

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 from routes.auth import router as auth_router
 from routes.groups import router as groups_router
@@ -8,7 +11,31 @@ app = FastAPI(
     title="EvenUp API",
     description="API for Group and Personal Expense Management",
     version="0.0.1",
+    docs_url=None,
+    redoc_url=None,
 )
+FAVICON_URL = "/static/EvenUp-white.svg"
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title="EvenUp Docs",
+        swagger_favicon_url=FAVICON_URL,
+    )
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title="EvenUp ReDoc",
+        redoc_favicon_url=FAVICON_URL,
+    )
+
 
 app.include_router(auth_router)
 app.include_router(users_router)
@@ -22,5 +49,5 @@ def health():
 
 
 @app.get("/")
-def EvenUp():
-    return {"message": "EvenUp"}
+def home():
+    return FileResponse("templates/index.html")
